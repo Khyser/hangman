50674ba099044e12012f97ec5d39967c7f6c3919
Khyser <38389762+Khyser@users.noreply.github.com>
2018-04-19 22:28:11 -0300

    Update hangman.sql

--- hangman game/hangman.sql
+++ hangman game/hangman.sql
@@ -28,7 +28,7 @@ CREATE TABLE `animals` (
   `tip` tinytext NOT NULL,
   `chances` tinyint(4) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,6 +37,7 @@ CREATE TABLE `animals` (
 
 LOCK TABLES `animals` WRITE;
 /*!40000 ALTER TABLE `animals` DISABLE KEYS */;
+INSERT INTO `animals` VALUES (1,'macaco','Costuma viver na selva e gosta de banana.',2);
 /*!40000 ALTER TABLE `animals` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -53,7 +54,7 @@ CREATE TABLE `fruits` (
   `tip` tinytext NOT NULL,
   `chances` tinyint(4) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -62,6 +63,7 @@ CREATE TABLE `fruits` (
 
 LOCK TABLES `fruits` WRITE;
 /*!40000 ALTER TABLE `fruits` DISABLE KEYS */;
+INSERT INTO `fruits` VALUES (1,'abacaxi','Símbolo das regiões tropicais',3),(2,'goiaba','Costuma ser verde por fora e rosa por dentro',4),(3,'abacate','Normalmente é verde tanto por fora quanto por dentro e tem um grande caroço/semente em seu interior',4),(4,'graviola','Lembra um pouco a jaca e começa com a letra g',4),(5,'pinha','Trata-se de uma fruta popularmente chamada de fruta-do-conde',5),(6,'tomate','É um fruto avermelhado que nós costumamos colocar na salada',2),(7,'uva','Fruta chave para se fazer vinho',2),(8,'morango','Trata-se de uma fruta que cientificamente NÃO é considerada uma fruta e sua cor é normalmente avermelhada',6),(9,'umbu','Origina-se de uma ávore popularmente conhecida como umbuzeiro',1),(10,'laranja','FRUTA USADA NO SUCO DO JAISON MENDES',1);
 /*!40000 ALTER TABLE `fruits` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -78,7 +80,7 @@ CREATE TABLE `objects` (
   `tip` tinytext NOT NULL,
   `chances` tinyint(4) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -87,6 +89,7 @@ CREATE TABLE `objects` (
 
 LOCK TABLES `objects` WRITE;
 /*!40000 ALTER TABLE `objects` DISABLE KEYS */;
+INSERT INTO `objects` VALUES (1,'vaso sanitario','É onde nois defeca',4),(2,'gardem gnome','Objetos presente no jogo Half-Life: Episode Two que garante a conquista Little Rocket Man',5),(3,'c4','Explosivo plastica bastante usado pelas forças dos EUA',4),(4,'rodo','Lembra a vassoura, porém este serve para tirar a água e não a poeira',2),(5,'onibus','Veiculo de transporte público com PNEUS!!!',4),(6,'balde','Recipiente grande para colocar água',2),(7,'isqueiro','Trata-se de um dispositivo portátil usado para criar chamas',3),(8,'isopor','É conhecido também pelo nome poliestireno e varia de sólido para o espumado',4),(9,'lampada incandescente','Dispositivo electrico que transforma energia elétrica em energia luminosa e energia térmica',4),(10,'mola','Trata-se de um objeto elastico com formato helicoidal que armazena energia mecância',2),(11,'brinquedo','Objeto usado por crianças para bricar e se divertir',2);
 /*!40000 ALTER TABLE `objects` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -100,14 +103,13 @@ DROP TABLE IF EXISTS `players_score`;
 CREATE TABLE `players_score` (
   `playerID` int(11) NOT NULL AUTO_INCREMENT,
   `nickname` varchar(21) NOT NULL,
-  `password` varchar(100) NOT NULL,
   `total_points` int(11) NOT NULL,
   `total_errors` int(11) NOT NULL,
   `total_wins` int(11) NOT NULL,
   `total_losses` int(11) NOT NULL,
   `total_matches` int(11) NOT NULL,
   PRIMARY KEY (`playerID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -116,6 +118,7 @@ CREATE TABLE `players_score` (
 
 LOCK TABLES `players_score` WRITE;
 /*!40000 ALTER TABLE `players_score` DISABLE KEYS */;
+INSERT INTO `players_score` VALUES (1,'Test',20,0,2,0,2),(2,'Test_2',24,4,1,0,1);
 /*!40000 ALTER TABLE `players_score` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -128,4 +131,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-04-14 22:56:49
+-- Dump completed on 2018-04-19 22:26:51
